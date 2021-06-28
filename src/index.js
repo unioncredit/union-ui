5d1033bb8ece98061cf1c0f50abb0b59df1fa644
Gerald <me@jacobford.co.uk>
2021-06-28 19:50:18 +0100

    feat: index exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./global.scss";
 
 export * from "./Button";
 export * from "./Box";
-export * from "./ButtonRow";
 export * from "./Text";
 export * from "./Heading";
-
+export * from "./Toggle";
+export * from "./Input";
+export * from "./Icon";
+export * from "./Control";
