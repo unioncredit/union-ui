29021d0a38e9dddd4078e026e24238a4c24e9da2
Gerald <me@jacobford.co.uk>
2021-12-13 22:42:11 +0000

    feat: alert spacing sizing

--- src/Alert/Alert.js
+++ src/Alert/Alert.js
@@ -4,22 +4,35 @@ import PropTypes from "prop-types";
 
 import { Text } from "../Text";
 import { Button } from "../Button";
+import { propsToStyles } from "../spacing";
 
 import "./alert.scss";
 
-export function Alert({ label, action, variant = "warning" }) {
+export function Alert({ label, action, size, variant, ...props }) {
   return (
-    <div className={cn("alert", { [`alert--${variant}`]: variant })}>
-      <Text ml="12px" mr="16px" mb={0}>
-        {label}
-      </Text>
-      <div className="alert__action">
-        <Button {...action} variant="secondary" />
-      </div>
+    <div
+      className={cn("alert", {
+        [`alert--${variant}`]: variant,
+        [`alert--${size}`]: size,
+      })}
+      style={propsToStyles(props)}
+    >
+      <Text mb={0}>{label}</Text>
+      {action && (
+        <div className="alert__action">
+          <Button {...action} variant="secondary" mr="16px" />
+        </div>
+      )}
     </div>
   );
 }
 
 Alert.propTypes = {
+  size: PropTypes.oneOf(["default", "small"]),
   variant: PropTypes.oneOf(["warning", "info", "success"]),
+};
+
+Alert.defaultProps = {
+  size: "default",
+  variant: "warning",
 };
