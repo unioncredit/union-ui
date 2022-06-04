5183761347de3a8e034ad03d21f57112eebf59a9
Gerald <gm@gerald.host>
2022-06-04 18:56:55 +0100

    feat: modal style tweaks

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -3,26 +3,27 @@ import PropTypes from "prop-types";
 import cn from "classnames";
 
 import { Card } from "../Card";
-import { Button } from "../Button";
+import { Box } from "../Box";
+import Close from "../icons/close.svg";
 
 import "./modal.scss";
 
 export function Modal({ children, onClose, title, size }) {
   return (
     <Card className={cn("modal")} size={size}>
-      {title && <Card.Header title={title} align="center" />}
-      <Card.Body>
-        {children}
+      <Box
+        align="center"
+        justify="space-between"
+        className="modalHeaderWrapper"
+      >
+        {title && <Card.Header title={title} align="left" />}
         {onClose && (
-          <Button
-            mt="10px"
-            variant="secondary"
-            onClick={onClose}
-            label="Close"
-            fluid
-          />
+          <Box mt="18px" className="closeWrapper">
+            <Close width="24px" height="24px" onClick={onClose} />
+          </Box>
         )}
-      </Card.Body>
+      </Box>
+      <Card.Body>{children}</Card.Body>
     </Card>
   );
 }
