1735382e3179e17b4ee937e56f631477508fa3bf
Gerald <gm@gerald.host>
2022-06-07 15:35:30 +0100

    feat: remove logo from navigation

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import cn from "classnames";
 
-import LogoSvg from "../icons/logo.svg";
-
 import "./nav.scss";
 
-export function Nav({ children, mobile, onLogoClick, logoComponent: Logo }) {
-  return (
-    <nav className={cn("nav", { "nav--mobile": mobile })}>
-      <div className="nav__logo">
-        <Logo width="32" onClick={onLogoClick} />
-      </div>
-      {children}
-    </nav>
-  );
+export function Nav({ children, mobile }) {
+  return <nav className={cn("nav", { "nav--mobile": mobile })}>{children}</nav>;
 }
 
 Nav.defaultProps = {
