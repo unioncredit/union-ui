cbcbedfc096eb8584df4b10b2f3a8190cd47483d
Gerald <gm@gerald.host>
2022-06-08 09:52:02 +0100

    feat: pagination and table styling

--- src/Table/Table.stories.jsx
+++ src/Table/Table.stories.jsx
@@ -12,6 +12,7 @@ import { Bar } from "../Bar";
 import { Skeleton } from "../Skeleton";
 import { Card } from "../Card";
 import { Box } from "../Box";
+import { Pagination } from "../Pagination/Pagination";
 
 export default {
   component: Table,
@@ -19,46 +20,49 @@ export default {
 };
 
 export const Default = () => (
-  <Table>
-    <TableRow>
-      <TableHead></TableHead>
-      <TableHead>Primary</TableHead>
-      <TableHead>Foo</TableHead>
-      <TableHead>Bar</TableHead>
-      <TableHead>Badge</TableHead>
-    </TableRow>
-    {Array(10)
-      .fill(0)
-      .map((_, i) => (
-        <TableRow
-          onClick={() => alert("clicked")}
-          active={i === 2 || i === 4}
-          error={i === 4 || i === 1}
-        >
-          <TableCell fixedSize>
-            <Avatar src="https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg" />
-          </TableCell>
-          <TableCell>
-            <Text>Primary Label</Text>
-          </TableCell>
-          <TableCell>
-            <Label as="p" size="small">
-              Small single label
-            </Label>
-          </TableCell>
-          <TableCell align="center">
-            <Bar size="small" percentage={60} label="62% No" secondaryBar />
-          </TableCell>
-          <TableCell align="right">
-            {i === 2 ? (
-              <Badge color="blue" label="Healthy" />
-            ) : (
-              <Badge color="red" label="Defaulted" />
-            )}
-          </TableCell>
-        </TableRow>
-      ))}
-  </Table>
+  <>
+    <Table>
+      <TableRow>
+        <TableHead></TableHead>
+        <TableHead>Primary</TableHead>
+        <TableHead>Foo</TableHead>
+        <TableHead>Bar</TableHead>
+        <TableHead>Badge</TableHead>
+      </TableRow>
+      {Array(10)
+        .fill(0)
+        .map((_, i) => (
+          <TableRow
+            onClick={() => alert("clicked")}
+            active={i === 2 || i === 4}
+            error={i === 4 || i === 1}
+          >
+            <TableCell fixedSize>
+              <Avatar src="https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg" />
+            </TableCell>
+            <TableCell>
+              <Text>Primary Label</Text>
+            </TableCell>
+            <TableCell>
+              <Label as="p" size="small">
+                Small single label
+              </Label>
+            </TableCell>
+            <TableCell align="center">
+              <Bar size="small" percentage={60} label="62% No" secondaryBar />
+            </TableCell>
+            <TableCell align="right">
+              {i === 2 ? (
+                <Badge color="blue" label="Healthy" />
+              ) : (
+                <Badge color="red" label="Defaulted" />
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
+    </Table>
+    <Pagination pages={4} activePage={4} mb="30px" onClick={() => {}} />
+  </>
 );
 
 export const TableCard = () => {
