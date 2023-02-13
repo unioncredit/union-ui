39e934be5911cfd5bc01463681ac17c9dffca182
0xbarbs <0xbarbs@proton.me>
2023-02-13 13:15:51 +0000

    feat: added set sizes to Avatar component

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -1,16 +1,57 @@
 import React from "react";
 
 import { Avatar } from "./Avatar";
+import {
+  ChartIcon,
+  CheckIcon,
+  CogIcon,
+  CrossIcon,
+  FilterIcon,
+  WalletIcon,
+} from "../Icons";
 
 export default {
   component: Avatar,
   title: "Components/Avatar",
 };
 
-export const Default = () => (
+const url = "https://pbs.twimg.com/profile_images/1576217831527616512/_iO9HGGE_400x400.jpg";
+
+export const All = () => (
   <>
-    <Avatar mb="12px" />
-    <Avatar size={48} mb="12px" variant="icon" icon={null} />
-    <Avatar size={48} mb="12px" variant="icon" icon={null} />
+    <Sizes />
+    <Custom />
+    <Icons />
   </>
 );
+
+export const Sizes = () => {
+  const sizes = ["x-small", "small", "medium", "large"];
+
+  return sizes.map((size) => (
+    <Avatar size={size} mb="12px" src={url} />
+  ))
+}
+
+export const Custom = () => {
+  const sizes = [16, 24, 40, 64];
+
+  return sizes.map((size) => (
+    <Avatar size={size} mb="12px" src={url} />
+  ))
+}
+
+export const Icons = () => {
+  const icons = [
+    ChartIcon,
+    CheckIcon,
+    CogIcon,
+    CrossIcon,
+    FilterIcon,
+    WalletIcon,
+  ];
+
+  return icons.map((icon) => (
+    <Avatar size="small" icon={icon} mb="12px" />
+  ))
+}
